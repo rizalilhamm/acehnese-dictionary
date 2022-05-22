fe1b7ba79c6af7bdbb4c1892cde708a05a8ee2f3
rizalilhamm <rizalilhamjob@gmail.com>
2022-05-22 15:33:21 +0700

    Autocomplete pada bagian pencarian

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template
 
 acehnese_dictionary = Blueprint('acehnese_dictionary', __name__)
 
@@ -6,3 +6,20 @@ acehnese_dictionary = Blueprint('acehnese_dictionary', __name__)
 @acehnese_dictionary.route('/home')
 def home():
     return "Halaman Home Berisi form input data"
+
+
+@acehnese_dictionary.route('/search')
+def search_data():
+    if request.method == 'GET':
+        languages = [
+                "A", "Alat", "Alamat", "Alokasi", "Apa", "Anda", "Andai", "Arak",
+                "B", "Bolu", "Bola", "Bunglon", "Badan", "Badai", "Bagaimana",
+                "C", "Contoh", "Corak", "Canda", "Ceramah", "Cari", "Cermat", "Cerdas",
+                "D", "Dolar", "Dunia", "Dari", "Doraemon", "Data", "Dan",
+                "E", "Ember", "Elemen", "Estetik",
+                "R", "Rumit", "Rencana", "Rupiah", "Roda", "Rugi", "Rangkul", "Raja", "Ratu", "Ragu"
+            ]
+        
+        if request.method == 'POST':
+            return 'Halaman Detail kosakata'
+        return render_template('search_vocabulary.html', languages=languages)
