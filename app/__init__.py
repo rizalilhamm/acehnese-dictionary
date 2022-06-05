76c08aaf8c3bb96a09436a3c0a580e06c805acce
rizalilhamm <rizalilhamjob@gmail.com>
2022-06-05 12:55:44 +0700

    Add: Flask-SQLAlchemy and database Migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from app.admin.views import admin
-from app.views import acehnese_dictionary
+# from app.views import acehnese_dictionary_blueprint
+from app.config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login_admin'
+from app.views import acehnese_dictionary_blueprint
 
 app.register_blueprint(admin)
-app.register_blueprint(acehnese_dictionary)
+app.register_blueprint(acehnese_dictionary_blueprint)
