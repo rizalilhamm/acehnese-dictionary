2f59ae702561a28e36228a0c2be9f0cacaa7ea3c
Rizal Ilham <rizalilham@Rizals-MacBook-Pro.local>
2022-08-04 01:11:29 +0700

    Do not render data from Database to make faster app

--- render_all_data.py
+++ render_all_data.py
@@ -6,23 +6,32 @@ Requirements:
 4. jika kata dalam list lebih 
 """
 
-import PyPDF2
-from app.controller.kosakata import buat_kosakata_baru
+def get_all_data():
+    file = open('kosa_kata.txt','r')
+    data = file.readlines()
+    semua_kosakata = []
+    semua_kosakata_aceh = []
+    semua_kosakata_indonesia = []
+    semua_kosakata_english = []
+    all_data = {}
 
-file = open('kosa_kata.txt','r')
+    for d in data:
+        kata = d.split(' - ')
+        if len(kata) < 2:
+            continue
+        if len(d.split(' - ')) > 3:
+            continue
+        if len(d.split(' - ')) < 3:
+            continue
+        
+        semua_kosakata.append([kata[0], kata[1], kata[2][:-1]])
+        semua_kosakata_aceh.append(kata[0])
+        semua_kosakata_indonesia.append(kata[1])
+        semua_kosakata_english.append(kata[2][:-1])
 
-data = file.readlines()
+    all_data['semua_kosakata'] = semua_kosakata
+    all_data['semua_kosakata_aceh'] = semua_kosakata_aceh
+    all_data['semua_kosakata_indonesia'] = semua_kosakata_indonesia
+    all_data['semua_kosakata_english'] = semua_kosakata_english
 
-for d in data:
-    kata = d.split(' - ')
-    if len(kata) < 2:
-        continue
-    if len(d.split(' - ')) > 3:
-        print('lebih dari 3')
-    if len(d.split(' - ')) < 3:
-        print('kurang dari 3')
-
-    # print(kata)
-    print(f'{kata[0]}, {kata[1]}, {kata[2]}')
-    buat_kosakata_baru(kata[0], kata[1], kata[2])
-    
+    return all_data
