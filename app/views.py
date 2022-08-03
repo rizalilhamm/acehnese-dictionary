070581199c8f655a7640c7eb417898e45fc26a4e
Rizal Ilham <rizalilham@Rizals-MacBook-Pro.local>
2022-08-04 01:12:10 +0700

    Render data from file to make faster app

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template
 from fast_autocomplete import AutoComplete
+from render_all_data import get_all_data
 
 from app.models import Vocabularies
 
@@ -14,23 +15,23 @@ def home():
 @acehnese_dictionary_blueprint.route('/search')
 @acehnese_dictionary_blueprint.route('/search?language=aceh-indonesia')
 def search_data():
+    semua_kosakata = get_all_data()
+
     if request.method == 'GET':
-        alldata = Vocabularies.query.all()
         searched_word = request.args.get('language')
         words = {}
-        for data in alldata:
-            # print('nilai data', data)
-            # words[str(data.aceh)] = {}
-            print('sedang mendapatkan data', data)        
-            if searched_word == 'indonesia-aceh':
-                words = {}
-                for data in alldata:
-                    words[str(data.indonesia)] = {}
 
-            elif searched_word == 'aceh-indonesia' or not searched_word:
-                words = {}
-                for data in alldata:
-                    words[str(data.aceh)] = {}
+        if searched_word == 'indonesia-aceh':
+            semua_kosakata_indonesia = semua_kosakata['semua_kosakata_indonesia']
+            print('nilai semua kosakata indonesia', semua_kosakata_indonesia)
+            for kosakata_indonesia in semua_kosakata_indonesia:
+                words[kosakata_indonesia] = {}
+        
+        elif not searched_word or searched_word == 'aceh-indonesia':
+            semua_kosakata_aceh = semua_kosakata['semua_kosakata_aceh']
+            print('nilai semua kosakata aceh', semua_kosakata_aceh)
+            for kosakata_aceh in semua_kosakata_aceh:
+                words[kosakata_aceh] = {}
 
         auto_complete = AutoComplete(words=words)
         auto_complete.search(word=searched_word, max_cost=3, size=3)
