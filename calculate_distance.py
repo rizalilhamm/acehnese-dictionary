0a7bc01162a34c17f06fb71e021cb972c4dc0b7e
Rizal Ilham <rizalilhamjob@gmail.com>
2022-08-16 22:27:00 +0700

    Calculate distance

--- calculate_distance.py
+++ calculate_distance.py
@@ -1,33 +1,35 @@
 from levenshtein import levenshteinDistanceMatrix
 
-def calcDictDistance(word, numWords, search_type=None):
+def calcDictDistance(word, numWords, search_type):
     file = open('kosa_kata.txt', 'r')
     lines = file.readlines()
     file.close()
 
     dictWordDist = []
     wordIdx = 0
+    perbedaan = []
 
     for line in lines:
         value = line.split(' - ')
         if len(value) != 3:
             continue
-        if search_type == 'aceh-indonesia':
-            # print('Nilai Line: ', value[0])
+        if search_type == 'aceh_indonesia':
             wordDistance = levenshteinDistanceMatrix(word, value[0].strip())
-        elif search_type == 'indonesia-aceh':
+            perbedaan.append(wordDistance)
+        elif search_type == 'indonesia_aceh':
             wordDistance = levenshteinDistanceMatrix(word, value[1].strip())
-            # print('Nilai Line: ', value[1])
+            perbedaan.append(wordDistance)
 
         if wordDistance >= 10:
             wordDistance = 9
         
-        if search_type == 'aceh-indonesia':
+        if search_type == 'aceh_indonesia':
             dictWordDist.append(str(int(wordDistance)) + "-" + value[0].strip())
-        elif search_type == 'indonesia-aceh':
+            perbedaan.append(wordDistance)
+
+        elif search_type == 'indonesia_aceh':
             dictWordDist.append(str(int(wordDistance)) + "-" + value[1].strip())
-
-
+            perbedaan.append(wordDistance)
         
         wordIdx += 1
 
@@ -36,16 +38,10 @@ def calcDictDistance(word, numWords, search_type=None):
     wordDetails = []
     currWordDist = 0
     dictWordDist.sort()
-    # print(dictWordDist)
     for i in range(numWords):
         currWordDist = dictWordDist[i]
         wordDetails = currWordDist.split("-")
         closestWords.append(wordDetails[1])
+
+    perbedaan.sort()
     return closestWords
-
-search_type = 'indonesia-aceh'
-# search_type = 'aceh-indonesia'
-
-for i in calcDictDistance('kamps', 2, search_type):
-    print(i)
-# print(type(calcDictDistance('text', 2)))
