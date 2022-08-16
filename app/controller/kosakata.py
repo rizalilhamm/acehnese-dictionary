c368d0614f2ba9450f3bdf1ffeeb248e86bf5ff3
Rizal Ilham <rizalilhamjob@gmail.com>
2022-08-16 22:29:12 +0700

    Comment kosakata.py it not used yet for now

--- app/controller/kosakata.py
+++ app/controller/kosakata.py
@@ -1,9 +1,9 @@
-from app import db
-from app.models import Vocabularies
+# from app import db
+# from app.models import Vocabularies
 
-db.create_all()
+# db.create_all()
 
-def buat_kosakata_baru(aceh, indonesia, english, real_aceh=None, jawoe=None):
-    kata_baru = Vocabularies(aceh=aceh, indonesia=indonesia, english=english, jawoe=jawoe)
-    db.session.add(kata_baru)
-    db.session.commit()
+# def buat_kosakata_baru(aceh, indonesia, english, real_aceh=None, jawoe=None):
+#     kata_baru = Vocabularies(aceh=aceh, indonesia=indonesia, english=english, jawoe=jawoe)
+#     db.session.add(kata_baru)
+#     db.session.commit()
