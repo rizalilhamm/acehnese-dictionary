ba5675c1650eba6a6683162f5a05e360a37a9d18
Rizal Ilham <rizalilhamjob@gmail.com>
2022-08-21 23:01:34 +0700

    GET DATABASE_URL

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ BASEDIR = os.path.dirname(PROJECT_ROOT)
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY') or 'ini-rahasia'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'aceh_dictionary.db')
+    SQLALCHEMY_DATABASE_URI =  os.getenv("DATABASE_URL") or 'sqlite:///' + os.path.join(BASEDIR, 'aceh_dictionary.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') or True
