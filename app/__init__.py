89b4c7316e874a50298b6f854a235f0ccbce8fc7
Rizal Ilham <rizalilhamjob@gmail.com>
2022-08-21 22:10:35 +0700

    App init

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_migrate import Migrate
 
 from app.admin.views import admin
+# from app.views import acehnese_dictionary_blueprint
+from app.config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login_admin'
 from app.views import acehnese_dictionary_blueprint
 
 app.register_blueprint(admin)
