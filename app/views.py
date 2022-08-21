da830413f5cf07407accbfe0bf5e86f84c0e0bdf
Rizal Ilham <rizalilhamjob@gmail.com>
2022-08-21 22:12:44 +0700

    Indonesia-Aceh detail and Aceh-Indonesia function

--- app/views.py
+++ app/views.py
@@ -1,19 +1,19 @@
-from crypt import methods
-from unittest import result
+from operator import index
 from flask import Blueprint, request, render_template
-from fast_autocomplete import AutoComplete
-from render_all_data import get_all_data
 from calculate_distance import calcDictDistance
+from app.models import Vocabularies
+from flask import jsonify
+
 
 acehnese_dictionary_blueprint = Blueprint('acehnese_dictionary_blueprint', __name__)
 
 @acehnese_dictionary_blueprint.route('/')
-@acehnese_dictionary_blueprint.route('/home')
+@acehnese_dictionary_blueprint.route('/home/')
 def home():
     return "Halaman Home Berisi form input data yang diinginkan"
 
 
-@acehnese_dictionary_blueprint.route('/search/aceh-indonesia', methods=["GET", "POST"])
+@acehnese_dictionary_blueprint.route('/search/aceh-indonesia/', methods=["GET", "POST"])
 def search_data_aceh_indonesia():
     semua_kosakata = {}
 
@@ -34,7 +34,7 @@ def search_data_aceh_indonesia():
     return render_template('search_vocabulary_aceh_indonesia.html')
 
 
-@acehnese_dictionary_blueprint.route('/search/indonesia-aceh', methods=["GET", "POST"])
+@acehnese_dictionary_blueprint.route('/search/indonesia-aceh/', methods=["GET", "POST"])
 def search_data_indonesia_aceh():
     semua_kosakata = {}
 
@@ -53,3 +53,23 @@ def search_data_indonesia_aceh():
 
         return render_template('search_vocabulary_indonesia_aceh.html', semua_kosakata=semua_kosakata, word=word)
     return render_template('search_vocabulary_indonesia_aceh.html')
+
+
+@acehnese_dictionary_blueprint.route('/search/indonesia-aceh/<string:kata>/', methods=["GET", "POST"])
+def get_indonesia_detail_indonesia_aceh(kata):
+    selected_word = Vocabularies.query.filter_by(indonesia=kata).first()
+
+    if selected_word is None:
+        return render_template("not_found_indonesia_aceh.html")
+    elif selected_word is not None:
+        return render_template('terjemahan_kata_indonesia_aceh.html', aceh=selected_word.aceh.upper(), indonesia=selected_word.indonesia.upper(), english=selected_word.english[:len(selected_word.english)-1].upper())
+
+
+@acehnese_dictionary_blueprint.route('/search/aceh-indonesia/<string:kata>/', methods=["GET", "POST"])
+def get_indonesia_detail_aceh_indonesia(kata):
+    selected_word = Vocabularies.query.filter_by(aceh=kata).first()
+
+    if selected_word is None:
+        return render_template("not_found_aceh_indonesia.html")
+    elif selected_word is not None:
+        return render_template('terjemahan_kata_aceh_indonesia.html', aceh=selected_word.aceh.upper(), indonesia=selected_word.indonesia.upper(), english=selected_word.english[:len(selected_word.english)-1].upper())
